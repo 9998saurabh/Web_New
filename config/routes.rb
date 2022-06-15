5335ec5cd0cc90afbef83fa2317867acd39c3ec1
Mayuri <mayuri@ubuntu18.04>
2022-06-15 13:56:10 +0530

    added controller root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
 
-  #get for get request and root is controller name and about is a page 
-  get "/about" , to: "root#about"
-  get "/contact",to: "root#contact"
-  get "/response", to: "root#api"
-  root "root#index"
-  
+  get 'root/index'
 end
